743e9df0218cda477b0782ed983dd0b4e98ad861
Tejal <tejaljaiswal004@gmail.com>
2026-07-09 20:28:38 +0530

    Added solution of LC2695

--- javascript/2635.js
+++ javascript/2635.js
@@ -7,3 +7,4 @@ var map = function (arr, fn) {
   }
   return NewArr;
 };
+//Take the element at position i from arr, pass it to the function fn along with its index i, then store the function's result in the same position i of NewArr.
