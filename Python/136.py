94a8cc80849bbb04b5e0442ebea84050387a86b6
Tejal <tejaljaiswal004@gmail.com>
2026-08-03 23:36:47 +0530

    add explanation for XOR approach

---
+++ Python/136.py
@@ -0,0 +1,11 @@
+# 136. Single Number
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        ans = 0
+        for num in nums:
+            ans=ans^num
+        return ans
