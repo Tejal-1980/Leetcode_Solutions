91d1834a909fcfd434ec35bba564d7f4ff592ee5
Tejal <tejaljaiswal004@gmail.com>
2026-08-08 17:04:54 +0530

    implement hashmap

---
+++ Python/1_Two_Sum.py
@@ -0,0 +1,16 @@
+# 1. Two Sum
+# You are given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.You may assume that each input would have exactly one solution, and you may not use the same element twice.You can return the answer in any order.
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        hashmap = {}
+        for i in range(len(nums)):
+            
+            needed= target-nums[i]
+            if needed in hashmap:
+                return[hashmap[needed],i]
+            hashmap[nums[i]]=i
---
+++ Python/Longest_subarr_with _sum_K.py
@@ -0,0 +1,1 @@
+# Given an array nums of size n and an integer k, find the length of the longest sub-array that sums to k. If no such sub-array exists, return 0.
