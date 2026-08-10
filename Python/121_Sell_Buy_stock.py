b757c4d40048339572e4c2131a7edfd0831066cd
Tejal <tejaljaiswal004@gmail.com>
2026-08-10 16:56:24 +0530

    Solved leetcode 121 using one-pass max

---
+++ Python/121_Sell_Buy_stock.py
@@ -0,0 +1,15 @@
+#121 Best time to buy & sell stock
+# You are given an array prices where prices[i] is the price of a given stock on the ith day.You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        min_price=prices[0]
+        max_profit=0
+        for price in prices:
+            min_price=min(min_price,price)
+            profit=price-min_price
+            max_profit=max(max_profit,profit)
+        return max_profit
--- Python/169_Majority_Element.py
+++ Python/169_Majority_Element.py
@@ -19,7 +19,7 @@ def majorityElement(self, nums):
   candidate=None
   count = 0
   for num in nums:
-    if count=0:
+    if count == 0:
       candidate = num
     if num == candidate:
       count+=1
