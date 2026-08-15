a6b4d89679190cdd2180e013f3025d40e6ab13f1
Tejal <tejaljaiswal004@gmail.com>
2026-08-15 17:02:21 +0530

    Solved Upper bound problem

---
+++ Binary_Search/Upper_bound.py
@@ -0,0 +1,18 @@
+class Solution:
+    def upperBound(self, nums, x):
+        n = len(nums)
+        low = 0
+        high = n - 1
+        ans = n
+
+        while low <= high:
+            mid = (low + high) // 2
+
+            if nums[mid] > x:
+                ans = mid
+                high = mid - 1
+            else:
+                low = mid + 1
+
+        return ans
+
