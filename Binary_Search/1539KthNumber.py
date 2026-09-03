ecf36c3178623d135fb5fa3cc4fb4aad42dc00bb
Tejal <tejaljaiswal004@gmail.com>
2026-09-03 10:02:33 +0530

    solve leetcode 1539 kth missing number

---
+++ Binary_Search/1539KthNumber.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def findKthPositive(self, arr, k):
+        """
+        :type arr: List[int]
+        :type k: int
+        :rtype: int
+        """
+        n=len(arr)
+        low=0
+        hhigh=n-1
+        while low<=high:
+            mid=(low+high)//2
+            missing = arr[mid]-mid+1
+            if  missing<k:
+                low=mid+1
+            else:
+                high=mid-1
+        return low + k
