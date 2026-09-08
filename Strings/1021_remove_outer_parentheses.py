05094567d4fa6a108cda26e9c1b680384691a4c7
Tejal <tejaljaiswal004@gmail.com>
2026-09-09 00:55:57 +0530

    solved leetcode question 1021

---
+++ Strings/1021_remove_outer_parentheses.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def removeOuterParentheses(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        result = []
+        count = 0
+
+        for ch in s:
+            if ch == '(':
+                if count > 0:
+                    result.append(ch)
+                count += 1
+            else:
+                count -= 1
+                if count > 0:
+                    result.append(ch)
+
+        return "".join(result)
